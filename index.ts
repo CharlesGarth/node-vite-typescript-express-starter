c7e8b44c861e1059ac627f2a2bb7408c17070a5b
CharlesGarth <charlesgarth@live.co.uk>
2023-06-29 17:02:01 +0100

    Hot reload server
    See: https://github.com/vitest-dev/vitest/pull/2792

--- index.ts
+++ index.ts
@@ -10,6 +10,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+if (import.meta.hot) {
+  import.meta.hot.on("vite:beforeFullReload", () => {
+    server.close();
+  });
+}
